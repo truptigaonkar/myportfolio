1b7d998ed1548384c0d78a852957d1892a86efeb
truptigaonkar <trups11.trupti@gmail.com>
2021-01-08 10:22:01 +0100

    updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <>
             <header id="header">
-                <div className="logo"><Link activeClass="active" to="home" spy={true} smooth={true} duration={100}>LOGO</Link></div>
+                <div className="logo"><Link activeClass="active" to="home" spy={true} smooth={true} duration={100}>VIKRANT GAONKAR</Link></div>
                 <ul>
                     <li><Link activeClass="active" to="home" spy={true} smooth={true} duration={100}>Home</Link></li>
                     <li><Link activeClass="active" to="about" spy={true} smooth={true} duration={100}>About</Link></li>
@@ -18,42 +18,32 @@ const Navbar = () => {
                 <div className="toggle"></div>
             </header>
             <section id="home">
-                <div>
-                    <h2>Responsive website design</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
-                    <Link activeClass="active" to="about" spy={true} smooth={true} duration={100}>Read More</Link>
-                </div>
+                <h2>Responsive website design</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
+                <Link activeClass="active" to="about" spy={true} smooth={true} duration={100}>Read More</Link>
             </section>
             <section id="about">
                 <div>
-                    <h2>About</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
+                <h2>About</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
                 </div>
                 <img src="../images/myphoto.JPG" alt="" />
             </section>
             <section id="services">
-                <div>
-                    <h2>Our Services</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
-                </div>
+                <h2>Our Services</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
             </section>
             <section id="portfolio">
-                <div>
-                    <h2>Portfolio</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
-                </div>
+                <h2>Portfolio</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
             </section>
             <section id="team">
-                <div>
-                    <h2>Our Team</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
-                </div>
+                <h2>Our Team</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
             </section>
             <section id="contact">
-                <div>
-                    <h2>Contact Us</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
-                </div>
+                <h2>Contact Us</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</p>
             </section>
         </>
     )
