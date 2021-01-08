7bb17b971181011b8ca8241c2e5373ac654add14
truptigaonkar <trups11.trupti@gmail.com>
2021-01-08 21:41:41 +0100

    Image carousal added at the home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { styled } from '@glitz/react';
 import { H2, P, Ul } from '../Reused/Generic';
 import { pseudo } from '@glitz/core';
+import aboutImage from '../images/image1.jpg'
+import image1 from '../images/image1.jpg'
+import image2 from '../images/image2.jpeg'
+import image3 from '../images/image3.jpeg'
+import image4 from '../images/image4.jpeg'
 
 const Navbar = () => {
     return (
@@ -18,16 +23,20 @@ const Navbar = () => {
                 </Ulnew>
             </Header>
             <Section id="home">
+                <Slide1 src={image1} alt="image1" />
+                <Slide2 src={image2} alt="image2" />
+                <Slide3 src={image3} alt="image3" />
+                <Slide4 src={image4} alt="image4" />
                 <H2new>Responsive website design</H2new>
                 <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</P>
                 <LinkA href="about">Read More</LinkA>
             </Section>
-            <Section id="about">
+            <Section id="about" style={{display: 'flex', flexDirection: 'row'}}>
                 <div>
                     <H2new>About</H2new>
                     <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cupiditate aspernatur quis esse quam nihil facilis reprehenderit, ratione omnis dolores nemo, ipsum exercitationem porro deleniti! Voluptas sit molestias rerum exercitationem.</P>
                 </div>
-                <img src="../images/myphoto.JPG" alt="" />
+                <img src={aboutImage} alt="aboutimage" className="aboutimage" width="300px" />
             </Section>
             <Section id="services">
                 <H2new>Our Services</H2new>
@@ -64,6 +73,7 @@ const Header = styled.header({
     alignItems: 'center',
     backgroundColor: '#111',
     zIndex: 1000,
+    opacity: '0.7',
 })
 
 const Logo = styled.div({
@@ -111,16 +121,80 @@ const Section = styled.section({
         x: '200px',
         y: '200px',
     },
-    ...pseudo(':nth-child(even)', 
-    {
-        position: 'relative',
-        justifyContent: 'center',
-        alignItems: 'center',
-    backgroundColor: '#111',
-    color: '#fff',
-    }),
+    ...pseudo(':nth-child(even)',
+        {
+            position: 'relative',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#111',
+            color: '#fff',
+        }),
 })
 
 const H2new = styled(H2, {
     textTransform: 'uppercase',
+})
+
+const Slide1 = styled.img({
+    display: 'block',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    animationName: {
+        '0%':  {opacity:0.4},
+        '25%':  {opacity:0},
+        '50%':  {opacity:0},
+        '75%':  {opacity:0},
+        '100%': {opacity:0.4} 
+    },
+    animationDuration: '20s',
+    animationIterationCount: 'infinite',
+})
+
+const Slide2 = styled.img({
+    display: 'block',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    animationName: {
+        '0%':  {opacity:0},
+        '25%':  {opacity:0.4},
+        '50%':  {opacity:0},
+        '75%':  {opacity:0},
+        '100%': {opacity:0.4} 
+    },
+    animationDuration: '20s',
+    animationIterationCount: 'infinite',
+})
+
+const Slide3 = styled.img({
+    display: 'block',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    animationName: {
+        '0%':  {opacity:0},
+        '25%':  {opacity:0},
+        '50%':  {opacity:0.4},
+        '75%':  {opacity:0},
+        '100%': {opacity:0} 
+    },
+    animationDuration: '20s',
+    animationIterationCount: 'infinite',
+})
+
+const Slide4 = styled.img({
+    display: 'block',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    animationName: {
+        '0%':  {opacity:0},
+        '25%':  {opacity:0},
+        '50%':  {opacity:0},
+        '75%':  {opacity:0.4},
+        '100%': {opacity:0} 
+    },
+    animationDuration: '20s',
+    animationIterationCount: 'infinite',
 })
